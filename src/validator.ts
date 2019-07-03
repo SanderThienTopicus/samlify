e69e320c4a196195629027c98c34100f239357cf
Sander Thien <sander.thien@topicus.nl>
2019-07-03 13:15:54 +0200

    geen validatie

--- src/validator.ts
+++ src/validator.ts
@@ -1,25 +1,5 @@
 function verifyTime(utcNotBefore?: string, utcNotOnOrAfter?: string): boolean {
-
-  const now = new Date();
-  if (!utcNotBefore && !utcNotOnOrAfter) {
     return true; // throw exception todo
-  }
-
-  let notBeforeLocal: Date | null = null;
-  let notOnOrAfterLocal: Date | null = null;
-
-  if (utcNotBefore && !utcNotOnOrAfter) {
-    notBeforeLocal = new Date(utcNotBefore);
-    return +notBeforeLocal <= +now;
-  }
-  if (!utcNotBefore && utcNotOnOrAfter) {
-    notOnOrAfterLocal = new Date(utcNotOnOrAfter);
-    return now < notOnOrAfterLocal;
-  }
-
-  notBeforeLocal = new Date(utcNotBefore!);
-  notOnOrAfterLocal = new Date(utcNotOnOrAfter!);
-  return +notBeforeLocal <= +now && now < notOnOrAfterLocal;
 }
 
 export {
