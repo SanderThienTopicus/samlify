5d4a0fbb6333ccfa9d09505c91ae1f02254cf71a
Sander Thien <sander.thien@topicus.nl>
2019-10-21 10:42:02 +0200

    versie upgrade

---
+++ types/index.d.ts
@@ -0,0 +1,10 @@
+export { default as IdentityProvider } from './src/entity-idp';
+export { default as ServiceProvider } from './src/entity-sp';
+export { default as IdPMetadata } from './src/metadata-idp';
+export { default as SPMetadata } from './src/metadata-sp';
+export { default as Utility } from './src/utility';
+export { default as SamlLib } from './src/libsaml';
+import * as Constants from './src/urn';
+import * as Extractor from './src/extractor';
+import { setSchemaValidator } from './src/api';
+export { Constants, Extractor, setSchemaValidator };
